2a35a6504605cf880c260b997a46a92d64691a1d
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-26 16:44:42 +0200

    [UPD] index semi-structure and added EventListener

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let calculate = document.getElementById('calculate'); 
+calculate.addEventListener('click', function () {
+    let kilometres = document.getElementById('kilometres').value
+    let age = document.getElementById('age').value
+    let price = kilometres * 0.21
+})
