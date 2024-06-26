b763c2ce0b609dd44602e95fc843fc47b8d36ef3
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-26 16:48:49 +0200

    [UPD] added if function into EventListener

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ calculate.addEventListener('click', function () {
     let kilometres = document.getElementById('kilometres').value
     let age = document.getElementById('age').value
     let price = kilometres * 0.21
+    if (age === 'minorenne') {
+        price = price - (price * 0.2)
+        console.log(price)
+    }
 })
