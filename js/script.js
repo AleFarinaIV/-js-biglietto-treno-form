c8e404166a1fd30f26f5cedcc369095a20a342fe
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-26 22:42:49 +0200

    [UPD] added img, updated index semi-structure and console.log for name and all prices

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-let calculate = document.getElementById('calculate'); 
+let calculate = document.getElementById('calculate');
 calculate.addEventListener('click', function () {
     let kilometres = document.getElementById('kilometres').value
     let age = document.getElementById('age').value
     let price = kilometres * 0.21
+    let name = document.getElementById('name').value
     if (age === 'minorenne') {
         price = price - (price * 0.2)
         console.log(price)
+        console.log(name)
+        document.getElementById('price').innerHTML = price
     }
     else if (age === 'senior') {
         price = price - (price * 0.4)
         console.log(price)
+        console.log(name)
     }
     else {
         console.log(price)
+        console.log(name)
     }
 })
